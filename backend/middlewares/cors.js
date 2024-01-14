21bc5714b22a2a7df97ef12b7e902ae75628cbc9
Ivan Lev <fanatos@mail.ru>
2024-01-14 22:13:35 +0500

    fix: add content type to preflight response

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -11,6 +11,7 @@ module.exports.cors = (req, res, next) => {
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Content-type', 'application/json');
     res.sendStatus(200);
   }
 
