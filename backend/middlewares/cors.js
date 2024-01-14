e34bfebc768bfebae6f19e440c7f82aab6177962
Ivan Lev <fanatos@mail.ru>
2024-01-14 22:29:19 +0500

    fix: add authorization geader to preflight request

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -11,12 +11,13 @@ module.exports.cors = (req, res, next) => {
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Headers', 'content-type')
+    res.header('Access-Control-Allow-Headers', 'content-type, authorization')
     res.sendStatus(200);
   }
 
   if (allowedCorsList.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.sendStatus(201);
   }
 
   next();
