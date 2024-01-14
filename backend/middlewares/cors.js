02201686118ab7ea77eebfc0b130f0007a614f00
Ivan Lev <fanatos@mail.ru>
2024-01-14 22:02:06 +0500

    fix: add allowed origin header to preflight request

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -10,7 +10,8 @@ module.exports.cors = (req, res, next) => {
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.sendStatus(200);
+    res.header('Access-Control-Allow-Origin', origin);
+    //res.sendStatus(200);
   }
 
   if (allowedCorsList.includes(origin)) {
