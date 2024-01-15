834bf82426b58066c556f3981223670f61d2e67f
Ivan Lev <fanatos@mail.ru>
2024-01-15 14:16:14 +0500

    fix: add missing semicolon

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -11,7 +11,7 @@ module.exports.cors = (req, res, next) => {
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Headers', 'content-type, authorization')
+    res.header('Access-Control-Allow-Headers', 'content-type, authorization');
     res.sendStatus(200);
   }
 
